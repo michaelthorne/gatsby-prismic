e5d25b30a7fb0fcfff8127dcc3f1c959be7af716
Michael Thorne <michael.thorne@gmail.com>
2019-03-08 14:18:34 +0200

    WIP

---
+++ gatsby-node.js
@@ -0,0 +1,38 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+
+const path = require('path')
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const pages = await graphql(`
+    {
+      allPrismicArticle {
+        edges {
+          node {
+            id
+            uid
+          }
+        }
+      }
+    }
+  `)
+
+  const template = path.resolve('src/templates/article.jsx')
+
+  pages.data.allPrismicArticle.edges.forEach(edge => {
+    createPage({
+      path: `/${edge.node.uid}`,
+      component: template,
+      context: {
+        uid: edge.node.uid,
+      },
+    })
+  })
+}
