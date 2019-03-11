f18f4f52eced93db1d05892cdf5bb3fc4eea96b5
Michael Thorne <michael.thorne@gmail.com>
2019-03-11 12:32:46 +0200

    Removed unused template

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,34 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-
-// const path = require('path')
-//
-// exports.createPages = async ({ graphql, actions }) => {
-//   const { createPage } = actions
-//
-//   const pages = await graphql(`
-//     {
-//       allPrismicFaq {
-//         edges {
-//           node {
-//             id
-//             uid
-//           }
-//         }
-//       }
-//     }
-//   `)
-//
-//   const template = path.resolve('src/templates/faq.js')
-//
-//   pages.data.allPrismicFaq.edges.forEach(edge => {
-//     createPage({
-//       path: `/${edge.node.uid}`,
-//       component: template,
-//       context: {
-//         uid: edge.node.uid,
-//       },
-//     })
-//   })
-// }
