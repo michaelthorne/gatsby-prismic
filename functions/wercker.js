4ca282450a9e52f7531ae62be5a306746ccfdb5d
Michael Thorne <michael.thorne@gmail.com>
2019-03-18 12:26:50 +0200

    Moved wercker access key into function

--- functions/wercker.js
+++ functions/wercker.js
@@ -5,7 +5,7 @@ exports.handler = (req, res) => {
     url: 'https://app.wercker.com/api/v3/runs/',
     method: 'POST',
     headers: {
-      'Authorization': `'Bearer ${process.env.WERCKER_ACCESS_TOKEN}'`,
+      'Authorization': `Bearer c69da77c1b8bbea3cfd06db208bd7d1271f11eaa5a74d80ad5fa049f2ae5bdd9`,
     },
     body: {
       'pipelineId': '5c8b6176c910281000b360d9',
